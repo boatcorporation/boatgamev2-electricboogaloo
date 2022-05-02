b688302afad3ca307ab90ca5d839aa97c7d8a0a6
Charlotte <charlotte.gayton@gmail.com>
2022-05-02 16:29:28 +0100

    Removed error .png from tests

--- tests/src/de/boatcorporation/gdxtesting/general/AssetTests.java
+++ tests/src/de/boatcorporation/gdxtesting/general/AssetTests.java
@@ -24,14 +24,14 @@ public class AssetTests {
 
     @Test
     public void ConstantineShipExists(){
-        String[] files = { "Constantine-ship-down.png", 
-                        "Constantine-ship-downleft.png",
-                        "Constantine-ship-downright.png",
-                        "Constantine-ship-left.png",
-                        "Constantine-ship-right.png",
-                        "Constantine-ship-up.png",
-                        "Constantine-ship-upleft.png",
-                        "Constantine-ship-upright.png" };
+        String[] files = { "Constantine-ship-down", 
+                        "Constantine-ship-downleft",
+                        "Constantine-ship-downright",
+                        "Constantine-ship-left",
+                        "Constantine-ship-right",
+                        "Constantine-ship-up",
+                        "Constantine-ship-upleft",
+                        "Constantine-ship-upright" };
 		for (String f : files) {
 			assertTrue("the file " + f + ".png does not exist",
 					Gdx.files.internal("../core/assets/ships/" + f + ".png").exists());
@@ -40,14 +40,14 @@ public class AssetTests {
 
     @Test 
     public void DerwentShipExists(){
-        String[] files = { "Derwent-ship-down.png",
-        "Derwent-ship-downleft.png",
-        "Derwent-ship-downright.png",
-        "Derwent-ship-left.png",
-        "Derwent-ship-right.png",
-        "Derwent-ship-up.png",
-        "Derwent-ship-upleft.png",
-        "Derwent-ship-upright.png" };
+        String[] files = { "Derwent-ship-down",
+        "Derwent-ship-downleft",
+        "Derwent-ship-downright",
+        "Derwent-ship-left",
+        "Derwent-ship-right",
+        "Derwent-ship-up",
+        "Derwent-ship-upleft",
+        "Derwent-ship-upright" };
 		for (String f : files) {
 			assertTrue("the file " + f + ".png does not exist",
 					Gdx.files.internal("../core/assets/ships/" + f + ".png").exists());
@@ -56,14 +56,14 @@ public class AssetTests {
 
     @Test 
     public void HalifaxShipExists(){
-        String[] files = { "Halifax-ship-down.png",
-        "Halifax-ship-downleft.png",
-        "Halifax-ship-downright.png",
-        "Halifax-ship-left.png",
-        "Halifax-ship-right.png",
-        "Halifax-ship-up.png",
-        "Halifax-ship-upleft.png",
-        "Halifax-ship-upright.png" };
+        String[] files = { "Halifax-ship-down",
+        "Halifax-ship-downleft",
+        "Halifax-ship-downright",
+        "Halifax-ship-left",
+        "Halifax-ship-right",
+        "Halifax-ship-up",
+        "Halifax-ship-upleft",
+        "Halifax-ship-upright" };
 		for (String f : files) {
 			assertTrue("the file " + f + ".png does not exist",
 					Gdx.files.internal("../core/assets/ships/" + f + ".png").exists());
