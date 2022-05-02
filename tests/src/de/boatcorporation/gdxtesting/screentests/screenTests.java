669bc8c1d70937b780d6f90086f1d60a3235b1f1
Charlotte <charlotte.gayton@gmail.com>
2022-05-02 17:04:05 +0100

    Added Screen tests file

---
+++ tests/src/de/boatcorporation/gdxtesting/screentests/screenTests.java
@@ -0,0 +1,25 @@
+
+package de.boatcorporation.gdxtesting.screentests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.badlogic.gdx.Gdx;
+
+import de.boatcorporation.gdxtesting.GdxTestRunner;
+
+
+@RunWith(GdxTestRunner.class)
+public class screenTests {
+    
+    @Test
+    public void WeatherExists() {
+		String[] files = { "weather" };
+		for (String f : files) {
+			assertTrue("the file " + f + ".png does not exist",
+					Gdx.files.internal("../core/assets/weather/" + f + ".png").exists());
+		}
+	}
+}
